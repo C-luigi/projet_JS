4eb98d85ac0df381355c900171d53482d2bb447a
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-07-26 09:58:33 +0200

    création bouton add

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -9,3 +9,17 @@ document.getElementById('column-view').addEventListener('click', function() {
     gallery.classList.remove('mosaic-view');
     gallery.classList.add('column-view');
 });
+function addImage() {
+    let url = document.getElementById('imageUrl').value;
+    fetch(url)
+        .then(response => {
+            if(response.ok) {
+                let img = document.createElement('img');
+                img.src = url;
+                document.getElementById('gallery').appendChild(img);
+            } else {
+                console.error('Erreur : URL invalide');
+            }
+        })
+        .catch(error => console.error('Erreur de fetch: ', error));
+}
