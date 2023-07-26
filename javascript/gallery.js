057d1f5a8bb65a343fe9ae87099ce7b59f1553f9
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-07-26 10:38:33 +0200

    vérification saisie

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -9,17 +9,25 @@ document.getElementById('column-view').addEventListener('click', function() {
     gallery.classList.remove('mosaic-view');
     gallery.classList.add('column-view');
 });
-function addImage() {
-    let url = document.getElementById('imageUrl').value;
-    fetch(url)
-        .then(response => {
-            if(response.ok) {
-                let img = document.createElement('img');
-                img.src = url;
-                document.getElementById('gallery').appendChild(img);
-            } else {
-                console.error('Erreur : URL invalide');
-            }
-        })
-        .catch(error => console.error('Erreur de fetch: ', error));
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('urlForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var url = document.getElementById('urlInput').value;
+        if (!(url.startsWith('http://') || url.startsWith('https://'))) {
+            alert('L\'URL doit commencer par "http://" ou "https://".');
+        } else {
+            fetch(url)
+                .then(response => {
+                    if(response.ok) {
+                        let img = document.createElement('img');
+                        img.src = url;
+                        document.getElementById('gallery').appendChild(img);
+                    } else {
+                        console.error('Erreur : URL invalide');
+                    }
+                })
+                .catch(error => console.error('Erreur de fetch: ', error));
+        }
+    });
+});
